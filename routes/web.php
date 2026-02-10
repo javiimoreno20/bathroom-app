0b69c652981a14f0fc3b21e5fa7c554d2b59352c
Javiimoreno20 <javiermm.04@gmail.com>
2026-02-10 12:12:29 +0100

    11 subida

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/run-migrations', function () {
+    \Artisan::call('migrate', ['--force' => true]);
+    \Artisan::call('db:seed', ['--force' => true]);
+    return "Migraciones ejecutadas";
+});
+
