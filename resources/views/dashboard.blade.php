a0dd8100fbe038eae9125d1a67a7523a14ec0cf5
Javiimoreno20 <javiermm.04@gmail.com>
2026-02-16 11:07:30 +0100

    septima prueba 16/02

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <div>
     <x-app.layout>
-        <x-dashboard :currentCount="$currentCount" :activePermissions="$activePermissions" :courses="$courses" :alumns="$alumns" :courseId="courseId"/>
+        <x-dashboard :currentCount="$currentCount" :activePermissions="$activePermissions" :courses="$courses" :alumns="$alumns" :courseId="$courseId"/>
     </x-app.layout>
 </div>
