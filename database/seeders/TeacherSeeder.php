9627103c4b7d06a4ca52b483f201c7dce6d6bc89
Javiimoreno20 <javiermm.04@gmail.com>
2026-02-16 12:57:22 +0100

    11 prueba 16/02

--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -14,12 +14,57 @@ class TeacherSeeder extends Seeder
     public function run(): void
     {
         //
-        DB::table('teachers')->insert([
-            'full_name' => 'javier',
-            'email' => 'javiermm.04@gmail.com',
-            'password' => Hash::make('1234'),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // DB::table('teachers')->insert([
+        //     'full_name' => 'javier',
+        //     'email' => 'javiermm.04@gmail.com',
+        //     'password' => Hash::make('1234'),
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+
+        //Ruta del CSV.
+        $path = database_path('data/teachers.csv');
+
+        //Si no encuentra el archivo lanza una excepción.
+        if (!file_exists($path)) {
+            throw new \Exception("El archivo CSV no existe en: {$path}");
+        }
+
+        //Abre el archivo csv.
+        $file = fopen($path, 'r');
+
+        //Leemos la cabecera.
+        $header = fgetcsv($file);
+
+        //Arrays para almacenar datos en batch.
+        $teachersData = [];
+
+        //Iniciamos un bucle del que no saldrá hasta que no queden líneas en el csv.
+        while (($row = fgetcsv($file)) !== false) {
+
+            $rowData = array_combine($header, $row);
+
+            //Divide las columnas del csv entre nombre de alumno y curso.
+            $full_name = trim($rowData['full_name']);
+            $email = trim($rowData['email']);
+            $password = trim($rowData['password']);
+
+            //Añadimos el alumno al array de batch.
+            $teachersData[] = [
+                'full_name' => $full_name,
+                'email' => $email,
+                'password' => Hash::make($password),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+        }
+
+        //Insertar los datos.
+        if (!empty($teachersData)) {
+            DB::table('teachers')->insert($teachersData);
+        }
+
+        //Cerramos el archivo csv.
+        fclose($file);
     }
 }
