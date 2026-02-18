15f58f042f42e678bb2758d0992885f2c1d39d8f
Javiimoreno20 <javiermm.04@gmail.com>
2026-02-18 12:12:20 +0100

    6 prueba 18/02

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BathroomPermissionController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Admin\ImportController;
+use App\Http\Controllers\admin\ImportController;
 
 //Cuando se inicia la app se redirige automáticamente al formulario de inicio de sesión o a la página de creación de permisos... según si está logueado o no.
 Route::get('/', function () {
