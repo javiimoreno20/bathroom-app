355afb7da10dde65de9111e44dd621cdcbdd83a4
Javiimoreno20 <javiermm.04@gmail.com>
2026-03-04 12:54:07 +0100

    7 prueba 04/03

---
+++ app/Http/Kernel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        // Puedes dejarlo vacío si quieres
+    ];
+
+    protected $middlewareGroups = [
+        'web' => [
+            \Illuminate\Session\Middleware\StartSession::class,
+        ],
+    ];
+
+    protected $routeMiddleware = [
+        'ProfesorAuth' => \App\Http\Middleware\ProfesorAuth::class,
+    ];
+}
