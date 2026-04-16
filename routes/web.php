f21868977151a550ca44266f8e9700e247a05b27
Javiimoreno20 <javiermm.04@gmail.com>
2026-04-16 13:25:09 +0200

    4 prueba 16/04

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\BathroomPermissionController;
 use App\Http\Controllers\admin\ImportController;
 use App\Http\Controllers\admin\AlumnController;
 use App\Http\Controllers\admin\TeacherController;
+use Illuminate\Http\Request;
 use App\Http\Controllers\SettingsController;
 
 /*
@@ -41,16 +42,11 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('teacher.auth')->group(function () {
 
-    /*
-    |--------------------------------------------------------------------------
-    | CAMBIO DE CONTRASEÑA (SOLO LOGUEADOS)
-    |--------------------------------------------------------------------------
-    */
-    Route::get('/teacher/change-password', [TeacherController::class, 'showChangePassword'])
-        ->name('teachers.change-password.form');
+Route::get('/teacher/change-password', [TeacherController::class, 'showChangePassword'])
+    ->name('teachers.change-password.form');
 
-    Route::post('/teacher/change-password', [TeacherController::class, 'changePassword'])
-        ->name('teachers.change-password');
+Route::post('/teacher/change-password', [TeacherController::class, 'changePassword'])
+    ->name('teachers.change-password');
 
     /*
     |--------------------------------------------------------------------------
@@ -58,73 +54,73 @@ Route::middleware('teacher.auth')->group(function () {
     |--------------------------------------------------------------------------
     */
 
-    Route::get('/dashboard', [BathroomPermissionController::class, 'index'])
-        ->name('dashboard');
+    Route::get('/dashboard', [BathroomPermissionController::class, 'index'])->name('dashboard');
 
-    Route::post('/give-permission', [BathroomPermissionController::class, 'givePermission'])
-        ->name('give.permission');
+    Route::post('/give-permission', [BathroomPermissionController::class, 'givePermission'])->name('give.permission');
 
-    Route::post('/mark-returned/{id}', [BathroomPermissionController::class, 'markReturned'])
-        ->name('mark.returned');
+    Route::post('/mark-returned/{id}', [BathroomPermissionController::class, 'markReturned'])->name('mark.returned');
 
-    Route::get('/permissions/history', [BathroomPermissionController::class, 'history'])
-        ->name('permissions.history');
+    Route::get('/permissions/history', [BathroomPermissionController::class, 'history'])->name('permissions.history');
 
-    Route::post('/export-permissions', [BathroomPermissionController::class, 'exportPermissions'])
-        ->name('permissions.export');
+   Route::post('/export-permissions', [BathroomPermissionController::class, 'exportPermissions'])->name('permissions.export');
+
 
     /*
     |--------------------------------------------------------------------------
-    | IMPORTACIÓN (SOLO ADMIN)
+    | IMPORTAR CSV
     |--------------------------------------------------------------------------
     */
-    Route::middleware(function ($request, $next) {
-        if (!session('profesor')?->is_admin) {
-            abort(403);
-        }
-        return $next($request);
-    })->group(function () {
 
-        Route::post('/import/{type}', [ImportController::class, 'import'])
-            ->name('import');
+    Route::post('/import/{type}', [ImportController::class, 'import'])->name('import');
 
-        /*
-        |--------------------------------------------------------------------------
-        | CONFIGURACIÓN (SOLO ADMIN)
-        |--------------------------------------------------------------------------
-        */
-        Route::post('/settings', [SettingsController::class, 'update'])
-            ->name('settings.update');
-
-        /*
-        |--------------------------------------------------------------------------
-        | PROFESORES (SOLO ADMIN)
-        |--------------------------------------------------------------------------
-        */
-
-        Route::get('/teachers', [TeacherController::class, 'index'])->name('teachers.index');
-        Route::get('/teachers/create', [TeacherController::class, 'create'])->name('teachers.create');
-        Route::post('/teachers', [TeacherController::class, 'store'])->name('teachers.store');
-        Route::get('/teachers/{id}/edit', [TeacherController::class, 'edit'])->name('teachers.edit');
-        Route::put('/teachers/{id}', [TeacherController::class, 'update'])->name('teachers.update');
-        Route::delete('/teachers/{id}', [TeacherController::class, 'destroy'])->name('teachers.destroy');
-
-    });
 
     /*
     |--------------------------------------------------------------------------
-    | ALUMNOS (LOGUEADOS)
+    | ALUMNOS
     |--------------------------------------------------------------------------
     */
 
     Route::get('/alumns', [AlumnController::class, 'index'])->name('alumns.index');
+
     Route::get('/alumns/create', [AlumnController::class, 'create'])->name('alumns.create');
+
     Route::post('/alumns', [AlumnController::class, 'store'])->name('alumns.store');
+
     Route::get('/alumns/{id}/edit', [AlumnController::class, 'edit'])->name('alumns.edit');
+
     Route::put('/alumns/{id}', [AlumnController::class, 'update'])->name('alumns.update');
+
     Route::delete('/alumns/{id}', [AlumnController::class, 'destroy'])->name('alumns.destroy');
 
+
+    /*
+    |--------------------------------------------------------------------------
+    | PROFESORES
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/teachers', [TeacherController::class, 'index'])->name('teachers.index');
+
+    Route::get('/teachers/create', [TeacherController::class, 'create'])->name('teachers.create');
+
+    Route::post('/teachers', [TeacherController::class, 'store'])->name('teachers.store');
+
+    Route::get('/teachers/{id}/edit', [TeacherController::class, 'edit'])->name('teachers.edit');
+
+    Route::put('/teachers/{id}', [TeacherController::class, 'update'])->name('teachers.update');
+
+    Route::delete('/teachers/{id}', [TeacherController::class, 'destroy'])->name('teachers.destroy');
+
+    Route::post('/teacher/change-password', [TeacherController::class, 'changePassword'])->name('teachers.change-password');
 });
+
+/*
+|--------------------------------------------------------------------------
+| CONFIGURACIÓN
+|--------------------------------------------------------------------------
+*/
+
+Route::post('/settings', [SettingsController::class, 'update'])->name('settings.update');
 
 /*
 |--------------------------------------------------------------------------
